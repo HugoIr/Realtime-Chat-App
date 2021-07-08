b9823390781b686556a275901617df087c3faf83
Hugo Irwanto <hugoirwanto23@gmail.com>
2021-07-08 14:32:02 +0700

    initial commit

---
+++ resources/js/Pages/Chat/messageContainer.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="h-96 w-full">
+            <div class="h-full p-2 flex flex-col-reverse overflow-scroll">
+                <div v-for="(message, index) in messages" :key="index">
+                    <message-item :message="message" />
+                </div>
+            </div>
+        </div>
+
+        <!-- <ul>
+            <li v-for="message in messages.slice().reverse()" :key="message.id">
+                {{ message.user.name }}
+                {{ message.message }}
+            </li>
+        </ul> -->
+        <!-- {{ messages }} -->
+    </div>
+</template>
+
+<script>
+import MessageItem from './messageItem.vue'
+export default {
+    props:['messages'],
+    components: {
+        MessageItem,
+    }
+}
+</script>
